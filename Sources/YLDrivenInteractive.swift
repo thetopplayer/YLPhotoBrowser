ee9290e3e4cc721213c388fb57288ba4027e2efe
朱云龙 <zhu_yunlong@czcg.com.cn>
2017-09-21 15:16:15 +0800

    no message

--- Sources/YLDrivenInteractive.swift
+++ Sources/YLDrivenInteractive.swift
@@ -22,7 +22,7 @@ class YLDrivenInteractive: UIPercentDrivenInteractiveTransition {
         }
     }
     
-    private var transitionContext: UIViewControllerContextTransitioning!
+    private var transitionContext: UIViewControllerContextTransitioning?
     private var blackBgView: UIView?
     private var fromView: UIView?
     private var toView: UIView?
@@ -31,6 +31,10 @@ class YLDrivenInteractive: UIPercentDrivenInteractiveTransition {
     private var isFirst = true
     
     func gestureRecognizeDidUpdate(_ gestureRecognizer: UIPanGestureRecognizer) {
+        
+        if transitionContext == nil {
+            return
+        }
         
         let translation = gestureRecognizer.translation(in:  gestureRecognizer.view?.superview)
         
@@ -75,10 +79,8 @@ class YLDrivenInteractive: UIPercentDrivenInteractiveTransition {
     
     func beginInterPercent() {
         
-        let transitionContext = self.transitionContext
-        
         // 转场过渡的容器view
-        if let containerView = transitionContext?.containerView {
+        if let containerView = self.transitionContext?.containerView {
             
             // ToVC
             let toViewController = transitionContext?.viewController(forKey: UITransitionContextViewControllerKey.to)
@@ -132,12 +134,10 @@ class YLDrivenInteractive: UIPercentDrivenInteractiveTransition {
         gestureRecognizer?.removeTarget(self, action: #selector(YLDrivenInteractive.gestureRecognizeDidUpdate(_:)))
         gestureRecognizer = nil
         
-        let transitionContext = self.transitionContext
-        
         fromView?.isHidden = true
         
         // 转场过渡的容器view
-        if let containerView = transitionContext?.containerView {
+        if let containerView = self.transitionContext?.containerView {
             
             // 过度的图片
             let transitionImgView = transitionImageView ?? UIImageView.init(image: transitionImage)
@@ -160,7 +160,7 @@ class YLDrivenInteractive: UIPercentDrivenInteractiveTransition {
                         self?.originalCoverView?.removeFromSuperview()
                         transitionImgView.removeFromSuperview()
                         
-                        transitionContext?.completeTransition(!(transitionContext?.transitionWasCancelled)!)
+                        self?.transitionContext?.completeTransition(!(self?.transitionContext?.transitionWasCancelled)!)
                         
                 })
                 
@@ -179,7 +179,7 @@ class YLDrivenInteractive: UIPercentDrivenInteractiveTransition {
                 self?.originalCoverView?.removeFromSuperview()
                 transitionImgView.removeFromSuperview()
                 
-                transitionContext?.completeTransition(!(transitionContext?.transitionWasCancelled)!)
+                self?.transitionContext?.completeTransition(!(self?.transitionContext?.transitionWasCancelled)!)
                 
             }
         }
